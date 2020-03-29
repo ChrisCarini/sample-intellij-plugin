988e0cd0a1d8d573097a54e765cbbcd92fe48159
Chris Carini <6374067+ChrisCarini@users.noreply.github.com>
2020-03-29 14:22:02 -0700

    Upgrading.
    Upgrading to IJ 2019.3
    Upgrading to IJ Gradle plugin 0.4.16
    Upgrading Gradle to 6.2

--- src/main/java/com/chriscarini/jetbrains/sampleintellijplugin/actions/HelloAction.java
+++ src/main/java/com/chriscarini/jetbrains/sampleintellijplugin/actions/HelloAction.java
@@ -1,6 +1,6 @@
-package com.chriscarini.sampleintellijplugin.actions;
+package com.chriscarini.jetbrains.sampleintellijplugin.actions;
 
-import com.intellij.icons.AllIcons;
+import com.intellij.ide.ui.ProductIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
@@ -9,7 +9,7 @@ import com.intellij.openapi.ui.Messages;
 public class HelloAction extends DumbAwareAction {
     public HelloAction() {
         super("Hello!");
-        getTemplatePresentation().setIcon(AllIcons.Icon_CEsmall);
+        getTemplatePresentation().setIcon(ProductIcons.getInstance().getProductIcon());
         getTemplatePresentation().setDescription("A simple hello.");
     }
 

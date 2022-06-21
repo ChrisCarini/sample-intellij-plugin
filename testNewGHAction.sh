e39352bac3a2464a26ce4dcc82ebcba3444d985c
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2022-06-20 18:05:47 -0700

    script improvements

--- testNewGHAction.sh
+++ testNewGHAction.sh
@@ -3,5 +3,14 @@
 pushd .github/actions/gh-test-ij-release-update-action/ || exit
 
 npm run build && npm run package
+WAS_BUILD_AND_PACKAGE_SUCCESS=$?
 
-popd && act -j test-gh-test-ij-release-update-action
+popd || exit
+
+if [[ $WAS_BUILD_AND_PACKAGE_SUCCESS -ne 0 ]]; then
+  echo "Building and packaging failure. See above."
+  exit 1
+fi
+
+echo "Building and packaging successful; running action..."
+act -j test-gh-test-ij-release-update-action

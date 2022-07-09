c97702b1074d4572de4b014b6b48e7d4e9d1fe46
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2022-07-09 00:21:38 -0700

    test

--- .github/actions/gh-test-ij-release-update-action/src/main.ts
+++ .github/actions/gh-test-ij-release-update-action/src/main.ts
@@ -1,36 +1,36 @@
-import * as core from '@actions/core'
-import {exec} from '@actions/exec'
-import * as github from '@actions/github'
-import {GitHub, getOctokitOptions} from '@actions/github/lib/utils'
+import * as core from "@actions/core";
+import { exec } from "@actions/exec";
+import * as github from "@actions/github";
+import { GitHub, getOctokitOptions } from "@actions/github/lib/utils";
 import {
   updateChangelog,
   updateGithubWorkflow,
   updateGradleProperties
-} from './jetbrains/files'
-import {getLatestIntellijRelease} from './jetbrains/versions'
-import * as semver from 'semver'
-import {wait} from './wait'
-import simpleGit, {StatusResult} from 'simple-git'
+} from "./jetbrains/files";
+import { getLatestIntellijRelease } from "./jetbrains/versions";
+import * as semver from "semver";
+import { wait } from "./wait";
+import simpleGit, { StatusResult } from "simple-git";
 
 async function checkFileChangeCount(): Promise<number> {
-  core.debug('BEFORE: simpleGit().status()')
-  const statusResult: StatusResult = await simpleGit().status()
-  core.debug('AFTER: simpleGit().status()')
+  core.debug("BEFORE: simpleGit().status()");
+  const statusResult: StatusResult = await simpleGit().status();
+  core.debug("AFTER: simpleGit().status()");
 
-  core.debug(`Files created:  ${statusResult.created.length}`)
-  core.debug(`Files modified: ${statusResult.modified.length}`)
-  core.debug(`Files deleted:  ${statusResult.deleted.length}`)
+  core.debug(`Files created:  ${statusResult.created.length}`);
+  core.debug(`Files modified: ${statusResult.modified.length}`);
+  core.debug(`Files deleted:  ${statusResult.deleted.length}`);
 
   statusResult.created.forEach(value => {
-    core.debug(`C --> ${value}`)
-  })
+    core.debug(`C --> ${value}`);
+  });
   statusResult.modified.forEach(value => {
-    core.debug(`M --> ${value}`)
-  })
+    core.debug(`M --> ${value}`);
+  });
   statusResult.deleted.forEach(value => {
-    core.debug(`D --> ${value}`)
-  })
-  return statusResult.modified.length
+    core.debug(`D --> ${value}`);
+  });
+  return statusResult.modified.length;
 }
 
 async function run(): Promise<void> {
@@ -46,53 +46,53 @@ async function run(): Promise<void> {
     // core.setOutput('time', new Date().toTimeString())
 
     // get the latest intellij release
-    const latestVersion: semver.SemVer = await getLatestIntellijRelease()
-    core.debug(`Latest IntelliJ Version: ${latestVersion}`)
+    const latestVersion: semver.SemVer = await getLatestIntellijRelease();
+    core.debug(`Latest IntelliJ Version: ${latestVersion}`);
 
     // update gradle.properties file
-    const currentPlatformVersion = await updateGradleProperties(latestVersion)
-    core.debug(`Current Platform Version: ${currentPlatformVersion}`)
+    const currentPlatformVersion = await updateGradleProperties(latestVersion);
+    core.debug(`Current Platform Version: ${currentPlatformVersion}`);
 
     // update CHANGELOG.md
-    await updateChangelog(latestVersion)
+    await updateChangelog(latestVersion);
 
     // update github workflows
-    await updateGithubWorkflow(currentPlatformVersion, latestVersion)
+    await updateGithubWorkflow(currentPlatformVersion, latestVersion);
 
-    core.debug('BEFORE: check file change count')
+    core.debug("BEFORE: check file change count");
     // check if there are files that are changed
-    const filesChanged: number = await checkFileChangeCount()
-    core.debug('AFTER: check file change count')
+    const filesChanged: number = await checkFileChangeCount();
+    core.debug("AFTER: check file change count");
 
     // If there are *NO* files that have changed, exit; we are done.
     if (filesChanged == 0) {
-      core.info('No files have changed, must be on latest version!')
-      return
+      core.info("No files have changed, must be on latest version!");
+      return;
     }
 
     // Commit the outstanding files
-    core.debug('ABOUT TO COMMIT')
-    const newBranchName = `ChrisCarini/upgradeIntelliJ-${latestVersion}`
+    core.debug("ABOUT TO COMMIT");
+    const newBranchName = `ChrisCarini/upgradeIntelliJ-${latestVersion}`;
 
-    const githubToken = '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' //core.getInput('GITHUB_TOKEN')
+    // const githubToken = core.getInput('MY_GITHUB_TOKEN')
     // core.setSecret(githubToken)
 
     // const githubUrl = github.context.serverUrl.split('//', 1)[1].trim()
-    const [githubUrlProtocol, githubUrl] = github.context.serverUrl.split('//')
-    const {owner, repo} = github.context.repo
-    const remoteRepo = `"https://${githubToken}@github.com/${owner}/${repo}.git"`
-    core.debug(`ORIGIN STR: ${remoteRepo}`)
-    core.debug(`SHOULD BE RUNNING: [git push ${remoteRepo} ${newBranchName}]`)
+    const [githubUrlProtocol, githubUrl] = github.context.serverUrl.split("//");
+    const { owner, repo } = github.context.repo;
+    // const remoteRepo = `"https://${githubToken}@github.com/${owner}/${repo}.git"`
+    // core.debug(`ORIGIN STR: ${remoteRepo}`)
+    // core.debug(`SHOULD BE RUNNING: [git push ${remoteRepo} ${newBranchName}]`)
     simpleGit()
       .checkoutLocalBranch(newBranchName)
-      .addConfig('http.sslVerify', 'false')
-      .addConfig('user.name', 'ChrisCarini')
-      .addConfig('user.email', '6374067+chriscarini@users.noreply.github.com')
+      .addConfig("http.sslVerify", "false")
+      .addConfig("user.name", "ChrisCarini")
+      .addConfig("user.email", "6374067+chriscarini@users.noreply.github.com")
       // .addConfig(
       //   'credential.https://github.com/.helper',
       //   '! f() { echo username=x-access-token; echo password=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; };f'
       // )
-      .commit(`Upgrading IntelliJ to ${latestVersion}`)
+      .commit(`Upgrading IntelliJ to ${latestVersion}`);
     // , async (err, data) => {
     //   core.debug(data.commit)
     //
@@ -122,9 +122,10 @@ async function run(): Promise<void> {
     //  Perhaps all my other attempts would work too - perhaps try pushing this
     //  action and depending upon it like a normal action and using it outside of
     //  the `act` CLI.........FFFFFFFFFFFFFAK
-    await exec('git', ['push', remoteRepo, newBranchName])
+    core.debug(`SHOULD BE RUNNING: [git push --set-upstream origin ${newBranchName}]`);
+    await exec(`git push --set-upstream origin ${newBranchName}`);
 
-    core.debug('COMMITTED!!!')
+    core.debug("COMMITTED!!!");
 
     // const octokit = github.getOctokit(githubToken)
     //
@@ -148,13 +149,14 @@ async function run(): Promise<void> {
     // // // wait a few seconds to wrap things up, I was seeing the above call not print anything
     // // await wait(10)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
 run()
-  .then(() => {})
-  .catch((err) => {
-    core.setFailed(err.message)
-    core.debug(err)
+  .then(() => {
   })
+  .catch((err) => {
+    core.setFailed(err.message);
+    core.debug(err);
+  });
